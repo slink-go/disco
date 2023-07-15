3999b1219af11966462c3b89df58fd20fd68ece9
Mikhail Kantur <kami@slink.ws>
2023-07-16 02:19:01 +1000

    multiarch docker build

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-Disco is a discovery service (a kind of eureka alternative).
+Disco is a discovery service (a kind of [eureka](https://github.com/Netflix/eureka) alternative) written in go.
+
+The simplest way to run this service is to use docker compose:
+
+```yaml
+services:
+  disco:
+    image: slinkgo/disco:alpine
+    container_name: disco
+    ports:
+      - "127.0.0.1:8080:8080"
+    environment:
+      - LOGGING_LEVEL=INFO
+```
+
+TODO: 
+- java client
+  - plain java
+  - spring boot starter
+- handle port-only endpoints from clients (remote-ip, X-CLIENT-IP, etc)
--- build.sh
+++ build.sh
@@ -2,9 +2,27 @@
 
 VERSION=$(cat VERSION)
 
+# https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/
+PLATFORMS=linux/arm/v7,linux/arm64/v8,linux/amd64
+
+function build_image() {
+  docker buildx build         \
+     -f Dockerfile.$2         \
+     --push                   \
+     --platform ${PLATFORMS}  \
+     --tag slinkgo/disco:$1 .
+}
+
 case $1 in
-  debian|alpine)
-    docker build -f Dockerfile.$1 --tag slink/disco:${VERSION}-$1 .
+  alpine)
+    build_image "${VERSION}-$1" $1
+    build_image "${VERSION}" $1
+    build_image "$1" $1
+    build_image latest $1
+  ;;
+  debian)
+    build_image "${VERSION}-$1" $1
+    build_image "$1" $1
   ;;
   *)
     echo "supported targets: debian, alpine"
