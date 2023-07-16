fd6e95cbc3de7d7cc4a6f80ba16bd17296ba91ab
Mikhail Kantur <kami@slink.ws>
2023-07-16 22:34:47 +1000

    wip

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-VERSION=$(cat VERSION)
+export DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${DIR}" || exit 1
+
+VERSION_SHORT=$(echo "$(cat ${DIR}/build/VERSION)")
+VERSION_LONG=$(echo "v$(cat ${DIR}/build/VERSION) ($(git describe --tags --abbrev=8 --dirty --always --long))")
 
 # https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/
 PLATFORMS=linux/arm/v7,linux/arm64/v8,linux/amd64
 
+function prepare() {
+  cat ${DIR}/build/logo.txt | sed -e "s/VERSION/${VERSION_LONG}/g" > ${DIR}/server/logo.txt
+}
 function build_image() {
   docker buildx build         \
      -f Dockerfile.$2         \
@@ -13,18 +20,27 @@ function build_image() {
      --tag slinkgo/disco:$1 .
 }
 
+
 case $1 in
+  prepare)
+    prepare
+  ;;
   alpine)
-    build_image "${VERSION}-$1" $1
-    build_image "${VERSION}" $1
+    prepare
+    build_image "${VERSION_SHORT}-$1" $1
+    build_image "${VERSION_SHORT}" $1
     build_image "$1" $1
     build_image latest $1
+    rm ${DIR}/src/logo.txt > /dev/null
   ;;
   debian)
-    build_image "${VERSION}-$1" $1
+    prepare
+    build_image "${VERSION_SHORT}-$1" $1
     build_image "$1" $1
+    rm ${DIR}/src/logo.txt > /dev/null
   ;;
   *)
     echo "supported targets: debian, alpine"
   ;;
 esac
+
